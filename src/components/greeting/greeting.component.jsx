c7551a0b509a337f7ee0da40aaedd4256facefff
Al_chemick <alex2015mig@gmail.com>
2021-02-01 00:20:35 +0200

    'adaptive_background'

--- src/components/greeting/greeting.component.jsx
+++ src/components/greeting/greeting.component.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { StyleSheet, css } from 'aphrodite';
 
 import s from './greeting.module.scss';
 
-const style = StyleSheet.create({
-        backImg: {
-        background: `fixed center/cover no-repeat url("${process.env.PUBLIC_URL}/img/code.jpg")`
-    }
-})
-
 function Greeting() {
     return (
-        <div className={`${s.header} ${css(style.backImg)}`} id="greeting">
+        <div className={s.header} id="greeting">
             <div className={s.header_container}>
                 <div>
                     <p className={s.p}>Hello, my name is Alex Kyrychenko and I am</p>
